1e572e0e143e748e9d257d615436921e09a7ee37
namseokkim <skatjr30@gmail.com>
2024-08-14 01:34:46 +0900

    feat  - Listener에 Async 추가

--- src/main/kotlin/kr/com/hhp/concertreservationapiserver/concert/presentation/event/ConcertReservationEventListener.kt
+++ src/main/kotlin/kr/com/hhp/concertreservationapiserver/concert/presentation/event/ConcertReservationEventListener.kt
@@ -3,6 +3,7 @@ package kr.com.hhp.concertreservationapiserver.concert.presentation.event
 import kr.com.hhp.concertreservationapiserver.concert.business.application.ConcertFacade
 import kr.com.hhp.concertreservationapiserver.concert.business.domain.event.ConcertEvent
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -10,12 +11,14 @@ import org.springframework.transaction.event.TransactionalEventListener
 @Component
 class ConcertReservationEventListener(private val concertFacade: ConcertFacade) {
 
+    @Async
     @EventListener
     fun handle(event: ConcertEvent.Reservation) {
         // 콘서트 임시 예약 이후 처리
         concertFacade.reserveSeat(concertSeatId = event.concertSeatId)
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handle(event: ConcertEvent.Pay) {
         // 콘서트 좌석 결제 이후 처리
